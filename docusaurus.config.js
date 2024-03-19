c18e3135b2e250d009d1da6f567d6e28cad54992
Torben Ewert <t.ewert@logic-joe.com>
2024-03-19 15:13:44 +0100

    update packages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,56 +1,54 @@
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const { themes } = require("prism-react-renderer");
 
 // With JSDoc @type annotations, IDEs can provide config autocompletion
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
-(
-  module.exports = {
-    title: "PiNC-Lang Docs",
-    tagline: "",
-    url: "https://pinc-lang.github.io",
-    baseUrl: "/pinc-docs/",
-    onBrokenLinks: "throw",
-    onBrokenMarkdownLinks: "warn",
-    favicon: "img/favicon.ico",
-    organizationName: "pinc-lang", // Usually your GitHub org/user name.
-    projectName: "pinc-docs", // Usually your repo name.
+module.exports = {
+  title: "pinc docs",
+  tagline: "",
+  url: "https://pinc-official.github.io",
+  baseUrl: "/pinc-docs/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "pinc-official", // Usually your GitHub org/user name.
+  projectName: "pinc-docs", // Usually your repo name.
+  staticDirectories: ["public", "static"],
 
-    presets: [
-      [
-        "@docusaurus/preset-classic",
-        /** @type {import('@docusaurus/preset-classic').Options} */
-        ({
-          docs: {
-            routeBasePath: "/",
-            sidebarPath: require.resolve("./sidebars.js"),
-            // Please change this to your repo.
-            // editUrl:
-            //   "https://github.com/facebook/docusaurus/edit/main/website/",
-          },
-          theme: {
-            customCss: require.resolve("./src/css/custom.css"),
-          },
-        }),
-      ],
-    ],
-
-    themeConfig:
-      /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-        navbar: {
-          title: "PiNC",
-          items: [
-            {
-              href: "https://github.com/pinc-lang",
-              label: "GitHub",
-              position: "right",
-            },
-          ],
+        docs: {
+          routeBasePath: "/",
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          // editUrl:
+          //   "https://github.com/facebook/docusaurus/edit/main/website/",
         },
-        prism: {
-          theme: lightCodeTheme,
-          darkTheme: darkCodeTheme,
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
-  }
-);
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "pinc",
+        items: [
+          {
+            href: "https://github.com/pinc-official",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      prism: {
+        theme: themes.github,
+        darkTheme: themes.dracula,
+      },
+    }),
+};
